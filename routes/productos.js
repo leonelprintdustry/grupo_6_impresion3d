efefe00a54ea7b8c7054cb0a70043e7ec79c6819
leonelprintdustry <leoneldonatini@hotmail.com>
2023-05-31 21:57:44 -0300

    Creamos la carepta data, models, productos.json y users.json

--- routes/productos.js
+++ routes/productos.js
@@ -3,7 +3,23 @@ const express = require('express');
 
 const router = express.Router();
 
-const productController = require('../controllers/productosController')
+const productController = require('../controllers/productosController');
+
+const multer = require('multer');
+
+const storage = multer.diskStorage({  
+    destination: (req, file, cb) => {
+        cb(null, './public/images/productos');
+    },
+    filename: (req, file, cb) => {
+        console.log(path.extname(file.originalname))
+        cb(null, Date.now() + '-' + file.originalname);
+    }
+});
+
+const upload = multer({ storage });
+
+router.post('/', upload.any('img'), productosController.XXXXXXXXX);
 
 router.get('/productCart', productController.getProductCart );
 
@@ -12,3 +28,4 @@ router.get('/productDetail',  productController.getProductDetail );
 router.get('/productAdd',  productController.getProductAdd );
 
 module.exports = router;
+
